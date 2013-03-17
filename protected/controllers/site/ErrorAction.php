fc2f297bdd26acaf7ddc8154e33265c5fecffe2e
otvorot <otvorot2008@gmail.com>
2013-03-17 17:47:23 +0400

    transaction failure exception add

--- protected/controllers/site/ErrorAction.php
+++ protected/controllers/site/ErrorAction.php
@@ -6,21 +6,27 @@ class ErrorAction extends CAction {
 			if(Yii::app()->request->isAjaxRequest){
 				if (isset($error['exception'])){
 					$exception = $error['exception'];
-					$result = array('code' => $exception->getCode(), 'message' => $exception->getMessage());
 					
-					if ($exception instanceof CHttpException){
-						$result['statusCode'] = $exception->statusCode;
-						if ($exception instanceof HttpException){
-							$result['type'] = $exception->type;
-							$result['context'] = $exception->context;
-							$result['specific']= $exception->specific;							
+					if (YII_DEBUG && isset(Yii::app()->request->restParams['verbose'])){
+						Yii::app()->displayException($exception);
+					}
+					else
+					{
+						$result = array('code' => $exception->getCode(), 'message' => $exception->getMessage());
+						if ($exception instanceof CHttpException){
+							$result['statusCode'] = $exception->statusCode;
+							if ($exception instanceof HttpException){
+								$result['type'] = $exception->type;
+								$result['context'] = $exception->context;
+								$result['specific']= $exception->specific;
+							}
 						}
-					}
-					else if ($exception instanceof CDbException){
-						$result['specicic'] = $exception->errorInfo;
-					}
+						else if ($exception instanceof CDbException){
+							$result['specicic'] = $exception->errorInfo;
+						}	
+						echo CJSON::encode($result);
+					} 
 					
-					echo CJSON::encode($result);
 					Yii::app()->end(1);
 				}
 			}
