61c9562ee5d8d4ec9ff1427052e2fe30490ca1e7
otvorot <otvorot2008@gmail.com>
2013-03-18 23:52:32 +0400

    update sql schema

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,3 +1,51 @@
+CREATE TABLE `company_table` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `issue_table` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `estimate` int(10) unsigned DEFAULT NULL,
+  `priority` int(10) unsigned DEFAULT NULL,
+  `value` int(10) unsigned DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `sprint_id` int(10) unsigned DEFAULT NULL,
+  `type` int(10) unsigned NOT NULL,
+  `dropped` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `product_table` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `company_id` int(10) unsigned DEFAULT NULL,
+  `dropped` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `user_issues_table` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `issue_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`issue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `user_projects_table` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
 CREATE TABLE `user_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `firstname` varchar(255) NOT NULL,
@@ -14,52 +62,7 @@ CREATE TABLE `user_table` (
   `session_count` int(11) unsigned DEFAULT '0',
   `active_project_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1$$
 
 
-CREATE TABLE `user_projects_table` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`,`product_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1$$
-
-CREATE TABLE `user_issues_table` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `issue_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`,`issue_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
-
-CREATE TABLE `product_table` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `company_id` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
-
-CREATE TABLE `issue_table` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `estimate` int(10) unsigned DEFAULT NULL,
-  `priority` int(10) unsigned DEFAULT NULL,
-  `value` int(10) unsigned DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `sprint_id` int(10) unsigned DEFAULT NULL,
-  `type` int(10) unsigned NOT NULL,
-  `dropped` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
-
-CREATE TABLE `company_table` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
 
